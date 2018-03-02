8037ce9186d5de9b69427657386ac29907639bac
Winson <rocketwings@gmail.com>
2018-03-02 14:40:49 -0800

    1.3e3 error achived

--- svm/matlab/eg_train.m
+++ svm/matlab/eg_train.m
@@ -16,14 +16,20 @@ prices = prices(:,1);
 n = 100;    %number of features
 test_num = 500; %test set size
 m = length(prices)-n-test_num;  %number of training instances
-start_index = 1;
+start_index = 3000;
 end_index = length(prices)-n;
 
-costs = [10];
-gamma = [0.01 0.1 1 10 100] .* (1/n);
+costs = [0.1 1 10 100];
+% gamma = [0.01 0.1 1 10 100] .* (1/n);
+gamma = 1/n * [0.1 1 10];
 
-for g = gamma
-for c = costs
+test_errors = zeros([length(costs), length(gamma)]);
+train_errors = zeros([length(costs), length(gamma)]);
+
+for g_idx = 1:length(gamma)
+for c_idx= 1:length(costs)
+    g = gamma(g_idx);
+    c = costs(c_idx);
     train_range = start_index:m;
     test_range = m+1:end_index;
 
@@ -40,10 +46,15 @@ for c = costs
     [test_pred_labels, test_accuracy, test_prob_estimates] = libsvmpredict(test_labels, test_set, svm_model);
     [train_pred_labels, train_accuracy, train_prob_estimates] = libsvmpredict(train_labels, train_set, svm_model);
 
-    usd_test_labels = (test_labels + all_means(test_range)) .* all_maxes; 
-    usd_train_labels = (train_labels + all_means(train_range)) .* all_maxes; 
-    usd_test_pred_labels = (test_pred_labels + all_means(test_range)) .* all_maxes; 
-    usd_train_pred_labels = (train_pred_labels + all_means(train_range)) .* all_maxes; 
+    % usd_test_labels = (test_labels + all_means(test_range)) .* all_maxes; 
+    % usd_train_labels = (train_labels + all_means(train_range)) .* all_maxes; 
+    % usd_test_pred_labels = (test_pred_labels + all_means(test_range)) .* all_maxes; 
+    % usd_train_pred_labels = (train_pred_labels + all_means(train_range)) .* all_maxes; 
+
+    usd_test_labels = (test_labels.* all_maxes + all_means(test_range)); 
+    usd_train_labels = (train_labels.* all_maxes + all_means(train_range)); 
+    usd_test_pred_labels = (test_pred_labels.* all_maxes + all_means(test_range)); 
+    usd_train_pred_labels = (train_pred_labels.* all_maxes + all_means(train_range)); 
 
     fig_count = fig_count +1;   figure(fig_count);    clf;
     subplot(2,1,1);
@@ -61,11 +72,15 @@ for c = costs
     plot(test_range, usd_test_pred_labels);
     plot(train_range, usd_train_labels);
     plot(train_range, usd_train_pred_labels);
-    plot(all_means.*all_maxes);
+    plot(all_means);
+    legend('actual test', 'prediction test', 'actual train', 'prediction train', 'means');
     title(sprintf('test and train usd, %s, n=%d',s,n));
 
-    test_error = sum(((test_labels-test_pred_labels)*all_maxes) .^2)/test_num ;
-    train_error = sum(((train_labels-train_pred_labels)*all_maxes) .^2)/m;
+    test_error = sum(((usd_test_labels-usd_test_pred_labels)) .^2)/test_num ;
+    train_error = sum(((usd_train_labels-usd_train_pred_labels)) .^2)/m;
+
+    test_errors(c_idx, g_idx) = test_error;
+    train_errors(c_idx, g_idx) = train_error;
     fprintf(sprintf('test_err=%3e\ttrain_err=%3e\tn=%d\ts=%s\n\n\n',test_error,train_error,n,s));
 end
 end
--- svm/matlab/series2features.m
+++ svm/matlab/series2features.m
@@ -9,15 +9,21 @@ for i = 1:len
     labels(i) = x(i+n);
 end
 % maxes = max(max(abs(instances),[],2));
-maxes = max(x);
-means = mean(instances./maxes,2);  %len x 1
+means = mean(instances, 2); %len x 1
+
+% maxes = max(abs(x));
+% means = mean(instances./maxes,2);  %len x 1
 
 
 
-instances = (instances./maxes) - means(:,ones(1,n));
+% instances = (instances./maxes) - means(:,ones(1,n));
+instances = instances - means(:,ones(1,n));
+maxes = max(max(abs(instances)));
+instances = instances ./ maxes;
 
-instances = (instances./maxes) - means(:,ones(1,n));
-labels = labels./maxes - means;
-instances = [means instances ];
+% instances = (instances./maxes) - means(:,ones(1,n));
+% labels = labels./maxes - means;
+labels = (labels - means) ./maxes;
+instances = [means./maxes instances ];
 
 end
