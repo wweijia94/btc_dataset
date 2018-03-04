0efd5a3bb39ae5bd4a74fa8f815c821d6bcde429
nrgodwin <nathanrgodwin@gmail.com>
2018-03-04 15:52:53 -0800

    Updated multivariate case to accept univariate

--- svm/matlab/series2features_mvar.m
+++ svm/matlab/series2features_mvar.m
@@ -1,4 +1,4 @@
-function [ instances, labels, maxes, means ] = series2features_mvar(x, n, d, testInd)
+function [ instances, labels, maxes, means ] = series2features_mvar(x, n, d,trainInd, testInd)
 %UNTITLED7 Summary of this function goes here
 %   n = number of features per instance
 %   d = prediction distance. default is 1
@@ -15,9 +15,7 @@ all_means = cell(1,size(x,2));
 for j = 1:size(x,2)
     [all_set{j}, all_labels{j}, all_maxes{j}, all_means{j} ] = series2features( x(:,j), n, d);
 end
-
-instances = cell2mat(all_set);
-%instances = reshape(all_set, size(x,1)-n, []);
+instances = all_set{trainInd};
 labels = all_labels{testInd};
 maxes = all_maxes{testInd};
 means = all_means{testInd};
